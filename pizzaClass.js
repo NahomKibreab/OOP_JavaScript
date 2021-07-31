e8e3fad3cff26b12ba992c3876f27c6d340cbaf4
Nahom Mehanzel <nknahom@gmail.com>
2021-07-31 02:15:25 +0000

    getter and setter in JS

--- pizzaClass.js
+++ pizzaClass.js
@@ -1,8 +1,6 @@
 class Pizza {
-  constructor(crust, size) {
+  constructor() {
     this.topping = ["cheese"];
-    this.crust = crust;
-    this.size = size;
   }
 
   addTopping(topping) {
@@ -13,8 +11,26 @@ class Pizza {
   removeTopping(topping) {
     this.topping.splice(this.topping.indexOf(topping), 1);
   }
+
+  set size(size) {
+    if (size === "s" || size === "m" || size === "l") {
+      this._size = size;
+    }
+  }
+
+  get size() {
+    return this._size;
+  }
+
+  get price() {
+    const basePrice = 10;
+    const toppingPrice = 2;
+    return basePrice + this.topping.length * toppingPrice;
+  }
 }
-const pizza1 = new Pizza();
-console.log(pizza1);
-pizza1.removeTopping("more cheese");
-console.log(pizza1.topping);
+const pizza = new Pizza();
+pizza.size = "s";
+
+pizza.addTopping("more cheese");
+pizza.size = "m";
+console.log(pizza.size);
